bf6e51ef41791e50d388c38256cc64aa9e20919d
sli <shasha.li@kisters.cn>
2017-12-22 14:35:13 +0800

    update package json file

--- src/helloworld.js
+++ src/helloworld.js
@@ -2,9 +2,13 @@
  * Created by sli on 12/22/17.
  */
  const  http = require("http");
+ const chalk = require("chalk");
 
  http.createServer(function(req, res) {
      res.writeHead(200, {'Content-Type': "text/plain"});
      res.end("hello World!")
  }).listen(8090)
+
+
+console.log(chalk.blue('hello world!!'))
 
